6271524661ff8f7472427bb25cfff79f0d7c6ed6
Sekkmer <sekkmer@gmail.com>
2018-12-19 13:41:18 +0100

    Update jog_transform.cpp

--- jog_transform.cpp
+++ jog_transform.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <ostream>
 #include <iostream>
 #include <fstream>
@@ -6,49 +8,61 @@
 #include <iomanip>
 #include <sstream>
 
+#include <filesystem>
+
 using namespace std;
 
 struct question {
-	string question, a, b, c, d;
+	string question;
+	string a, b, c, d;
 };
 
-string quote(const string & str){
+string quote(const string & str) {
 	stringstream ss;
 	ss << std::quoted(str);
 	return ss.str();
 }
 
 std::ostream & operator<<(std::ostream & os, const question & q) {
-    os << "{";
+	os << "{";
 	os << "\"question\":" << quote(q.question) << ",";
 	os << "\"a\":" << quote(q.a) << ",";
 	os << "\"b\":" << quote(q.b) << ",";
 	os << "\"c\":" << quote(q.c) << ",";
 	os << "\"d\":" << quote(q.d) << "";
-    os << "}";
+	os << "}";
 	return os;
 }
 
 int main() {
+	cout << filesystem::current_path() << endl;
+
 	ifstream file("Jog_tesztkerdesek.txt");
-	file.exceptions(ifstream::failbit | ifstream::badbit);
+	//file.exceptions(ifstream::failbit | ifstream::badbit);
 
 	cout << "OK File" << endl;
 
 	string line;
 	char state = 'x';
 	string key;
-	int number, last = 0;
-	vector<question> vec(147);
+	int number = 0, last = 0;
+	vector<question> vec(184);
 	auto * que = &vec[0];
 	while (getline(file, line)) {
-		cout << key << endl;
+		//cout << key << "" << number<< endl;
+	re_case:;
 		switch (state) {
 		case 'q':
-		    que->question = line;
+			que->question += line;
 			state = 'a';
 			break;
 		case 'a':
+			if (line[0] != 'a' || line[1] != ')' || line[2] != ' ') {
+				cout << "maybe error: " << line << endl;
+				que->question += " ";
+				state = 'q';
+				goto re_case;
+			}
 			que->a = line.substr(line.find(' '));
 			state = 'b';
 			break;
@@ -65,25 +79,42 @@ int main() {
 			state = 'x';
 			break;
 		case 'x':
-			if (number == 147) {
+			if (number == 183) {
 				goto print;
 			}
-			if (sscanf(line.c_str(), "%d.\n", &number) == 1 && number == last + 1) {
-				last = number;
+			if (last < 170) {
+				if (sscanf(line.c_str(), "%d.\n", &number) == 1 && number == last + 1) {
+					last = number;
+					key = to_string(number);
+					que = &vec[number - 1];
+					state = 'q';
+				}
+			} else if (last == 170) {
+				if (line.substr(0, 6) == "170/A.") {
+					last = number + 1;
+					key = "170/A";
+					que = &vec[number];
+					state = 'q';
+				}
+			} else if (sscanf(line.c_str(), "%d.\n", &number) == 1 && number == last) {
+				last = number + 1;
 				key = to_string(number);
-				que = &vec[number-1];
+				que = &vec[number];	
 				state = 'q';
 			}
+
 			break;
 		}
 	}
 
 	cout << "OK Read" << endl;
-	print:  
+print:
+	cout << "OK Read" << endl;
+
 	ofstream data_json;
 	data_json.open("data.json");
-    data_json << "{\"" << 1 << "\":" << vec[0];
-	for(int i = 1; i < vec.size(); ++i) {
+	data_json << "{\"" << 1 << "\":" << vec[0];
+	for (int i = 1; i < vec.size(); ++i) {
 		data_json << ",\"" << (i + 1) << "\":" << vec[i];
 	}
 	data_json << "}";
